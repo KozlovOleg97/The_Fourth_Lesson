b28c8c40d7753715faebb8ffaa2b08bb01a9fc20
KozlovOleg97 <kozlovoleg2021@gmail.com>
2022-11-03 23:14:24 +0300

    Creating matrix

--- Program.cs
+++ Program.cs
@@ -1,22 +1,31 @@
 ﻿
-int[] nums = { -15, -20, 1, 2, 3, 4, 5};
-
-int temp;
-for (int i = 0; i < nums.Length - 1; i++)
+int length; //длинна матрицы или строка
+int height; //высота матрицы или столбец
+int sum = 0;  //сумма чисел, заполняющих матрицу
+Console.Write("Введите длину матрицы              : ");
+length = int.Parse(Console.ReadLine());
+Console.Write("Введите высоту матрицы             : ");
+height = int.Parse(Console.ReadLine());
+Console.WriteLine("\nЗаполнение матрицы\n");
+int[,] ara = new int[length, height];
+for (int i = 0; i < length; i++)
 {
-    for (int j = i + 1; j <nums.Length; j++)
+    for (int j = 0; j < height; j++)
     {
-        if (nums[i] == nums[j])
-        {
-            temp = nums[i];
-            nums[i] = nums[j];
-            nums[j] = temp;
-        }
+        Console.Write("Введите число                      : ");
+        ara[i, j] = int.Parse(Console.ReadLine());
     }
 }
-
-Console.WriteLine("Output :");
-for (int i = 0; i < nums.Length; i++)
+Console.WriteLine();
+for (int i = 0; i < length; i++)
 {
-    Console.WriteLine(nums[i]);
+    for (int j = 0; j < height; j++)
+    {
+        Console.Write("{0} ", ara[i, j]);
+        sum += ara[i, j];
+    }
+    Console.WriteLine();
 }
+Console.WriteLine("\nСумма матрицы = {0}", sum);
+Console.ReadKey();
+return 0;
