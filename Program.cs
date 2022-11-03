b069d1d17a03a710e2968cf62ca3fb6048ee906b
KozlovOleg97 <kozlovoleg2021@gmail.com>
2022-11-03 23:12:11 +0300

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿
+int[] nums = { -15, -20, 1, 2, 3, 4, 5};
+
+int temp;
+for (int i = 0; i < nums.Length - 1; i++)
+{
+    for (int j = i + 1; j <nums.Length; j++)
+    {
+        if (nums[i] == nums[j])
+        {
+            temp = nums[i];
+            nums[i] = nums[j];
+            nums[j] = temp;
+        }
+    }
+}
+
+Console.WriteLine("Output :");
+for (int i = 0; i < nums.Length; i++)
+{
+    Console.WriteLine(nums[i]);
+}
