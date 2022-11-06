a33821fc572ce26d45feaa19355b2342fa4967b7
KozlovOleg97 <kozlovoleg2021@gmail.com>
2022-11-06 23:13:57 +0300

    Adding new math operations

--- Program.cs
+++ Program.cs
@@ -1,18 +1,27 @@
 ﻿
-int length; //длинна матрицы или строка
-int height; //высота матрицы или столбец
-int sum = 0;  //сумма чисел, заполняющих матрицу
-Console.Write("Введите длину матрицы              : ");
+
+using System;
+using The_Fourth_Lesson;
+
+
+
+
+int length; 
+int height; 
+int sum = 0;  
+Console.Write("Enter the length of the matrix: ");
 length = int.Parse(Console.ReadLine());
-Console.Write("Введите высоту матрицы             : ");
+
+Console.Write("Enter the height of the matrix: ");
 height = int.Parse(Console.ReadLine());
-Console.WriteLine("\nЗаполнение матрицы\n");
+
+Console.WriteLine("\nFillin in the matrix\n");
 int[,] ara = new int[length, height];
 for (int i = 0; i < length; i++)
 {
     for (int j = 0; j < height; j++)
     {
-        Console.Write("Введите число                      : ");
+        Console.Write("Enter a number: ");
         ara[i, j] = int.Parse(Console.ReadLine());
     }
 }
@@ -21,11 +30,33 @@ for (int i = 0; i < length; i++)
 {
     for (int j = 0; j < height; j++)
     {
+        
         Console.Write("{0} ", ara[i, j]);
         sum += ara[i, j];
     }
     Console.WriteLine();
 }
-Console.WriteLine("\nСумма матрицы = {0}", sum);
+Console.WriteLine("\nSum of the matrix = {0}", sum);
 Console.ReadKey();
-return 0;
+
+
+MathOperations mathOp = new MathOperations();
+
+Console.WriteLine("Choose the action.");
+Console.WriteLine("'p' - amount of positive and negative numbers");
+Console.WriteLine("'s' - array sorting");
+Console.WriteLine("'i' - array inversion");
+char act = Convert.ToChar(Console.ReadLine());
+
+switch (act)
+{
+    case 'p':
+        mathOp.AmountOfPosAndNegNumbers(ara, length, height);
+        break;
+    case 's':
+        mathOp.ArrSort(ara, length, height);
+        break;
+    case 'i':
+        mathOp.Invers(ara, length, height);
+        break;
+}
